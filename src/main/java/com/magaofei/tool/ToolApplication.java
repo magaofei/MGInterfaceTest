148ee18df8f86e62aac19944cb7262f8bea79aaa
Mark <mamian521@gmail.com>
2018-01-21 23:11:35 +0800

    author

--- src/main/java/com/magaofei/tool/ToolApplication.java
+++ src/main/java/com/magaofei/tool/ToolApplication.java
@@ -1,5 +1,10 @@
 package com.magaofei.tool;
 
+/**
+ * @Author mark
+ *
+ */
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
